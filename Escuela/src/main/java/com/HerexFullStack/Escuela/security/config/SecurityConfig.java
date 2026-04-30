3f30648605ebdc06c12abe061f5d74c39134a3c4
Jesús <jhernandezrexa@gmail.com>
2026-04-30 16:42:28 +0200

    Authentication manager creado.

--- Escuela/src/main/java/com/HerexFullStack/Escuela/security/config/SecurityConfig.java
+++ Escuela/src/main/java/com/HerexFullStack/Escuela/security/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.HerexFullStack.Escuela.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,4 +22,13 @@ public class SecurityConfig {
                 .formLogin(form -> form.permitAll())
                 .build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration){
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
 }
+
+
---
+++ Escuela/src/main/java/com/HerexFullStack/Escuela/service/UserDetailsServiceImp.java
@@ -0,0 +1,9 @@
+package com.HerexFullStack.Escuela.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImp {
+
+
+}
