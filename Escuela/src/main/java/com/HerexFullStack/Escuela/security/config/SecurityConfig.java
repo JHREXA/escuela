9c583375778bbb564b9396e83d7c2f6a4169768e
Jesús <jhernandezrexa@gmail.com>
2026-04-30 13:52:28 +0200

    Autenticación con usuario y contraseña realizada.

---
+++ Escuela/src/main/java/com/HerexFullStack/Escuela/security/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.HerexFullStack.Escuela.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity)throws Exception{
+        return httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/holanoseg").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.permitAll())
+                .build();
+    }
+}
