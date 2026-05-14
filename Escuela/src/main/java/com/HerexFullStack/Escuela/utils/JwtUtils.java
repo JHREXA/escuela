5bf966f8b1b07f2a0f6f268b3fb273ecf8074e48
Jesús <jhernandezrexa@gmail.com>
2026-05-14 14:19:58 +0200

    Method for creating tokens done.

--- Escuela/src/main/java/com/HerexFullStack/Escuela/utils/JwtUtils.java
+++ Escuela/src/main/java/com/HerexFullStack/Escuela/utils/JwtUtils.java
@@ -1,9 +1,45 @@
 package com.HerexFullStack.Escuela.utils;
 
+import com.auth0.jwt.JWT;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtUtils {
 
+    @Value("${security.jwt.private.key}")
+    private String privateKey;
+    @Value("${security.jwt.user.generator}")
+    private String userGenerator;
 
+    public String createToken(Authentication autentication){
+
+        Algorithm algorithm = Algorithm.HMAC256(privateKey);
+
+        String username = autentication.getPrincipal().toString();
+
+        String authorities = autentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        String jwtToken = JWT.create()
+                .withIssuer(this.userGenerator)
+                .withSubject(username)
+                .withClaim("authorities", authorities)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 18000000))
+                .withJWTId(UUID.randomUUID().toString())
+                .withNotBefore(new Date(System.currentTimeMillis()))
+                .sign(algorithm);
+
+            return jwtToken;
+    }
 }
