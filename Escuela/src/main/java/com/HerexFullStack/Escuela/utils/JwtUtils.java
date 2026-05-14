d8f06cc7b25f5fcd9ee7a816fa8224d38758962f
Jesús <jhernandezrexa@gmail.com>
2026-05-14 15:43:51 +0200

    Method for validating tokens created.

--- Escuela/src/main/java/com/HerexFullStack/Escuela/utils/JwtUtils.java
+++ Escuela/src/main/java/com/HerexFullStack/Escuela/utils/JwtUtils.java
@@ -1,6 +1,9 @@
 package com.HerexFullStack.Escuela.utils;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -41,5 +44,20 @@ public class JwtUtils {
                 .sign(algorithm);
 
             return jwtToken;
+    }
+
+    public DecodedJWT validateToken(String token){
+
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(privateKey);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(userGenerator)
+                    .build();
+
+            DecodedJWT decodedJWT = verifier.verify(token);
+            return decodedJWT;
+        }catch (JWTVerificationException exception){
+            throw new JWTVerificationException("Invalid token. Not authorized.");
+        }
     }
 }
